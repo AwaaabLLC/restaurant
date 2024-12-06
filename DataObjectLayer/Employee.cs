f749109849b6a9df60c00b90274898cce90d2524
AwaabElamin <AwaabElamin@gmail.com>
2024-12-05 21:25:39 -0600

    update data objects layer

--- DataObjectLayer/Employee.cs
+++ DataObjectLayer/Employee.cs
@@ -6,11 +6,12 @@ using System.Threading.Tasks;
 
 namespace DataObjectLayer
 {
-    public class User
+    public class Employee
     {
         public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CellPhone { get; set; }
         public string EmailAddress { get; set; }
-        public string Password { get; set; }
-        public bool Active { get; set; }
     }
 }
